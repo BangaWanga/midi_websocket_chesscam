2581e997c1f54e10e1a26309e62179afea285b1f
BangaWanga <nicolas.c.schilling@googlemail.com>
2022-03-29 21:37:45 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rz-hub",
-    version="0.2",
+    version="0.3",
     packages=find_packages(),
-    install_requires=['websockets', 'PyQt5', "pygame", "numpy", "cv2"],
+    install_requires=['websockets', 'PyQt5', "pygame", "numpy", "opencv-python"],
 )
