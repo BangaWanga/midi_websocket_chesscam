4bb6cab13f56934bee11b3bbe35052e76215eeeb
Nicolas Christian Schilling <nicoals_christian.schilling@tu-dresden.de>
2022-05-27 17:19:49 +0200

    Threading

--- Sequencer/main.py
+++ Sequencer/main.py
@@ -1,14 +1,16 @@
 from sequencer import sequencer
 import asyncio
+import threading
 
 
 async def main():
     s = sequencer(4)
-    await asyncio.gather(
-        s.run(),
-        s.handle_network_connection(),
-    )
-
+    t0 = threading.Thread(target=s.run, args=())
+    t1 = threading.Thread(target=s.handle_network_connection, args=())
+    t0.start()
+    t1.start()
+    t0.join()
+    t1.join()
 
 if __name__ == "__main__":
     main()
